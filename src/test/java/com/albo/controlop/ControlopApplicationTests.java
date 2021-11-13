5b8e7d6abda6f2851afbf1fd5ef5a2ccb9a87cb1
Danny <live626@gmail.com>
2021-11-13 19:49:04 -0400

    Started project

---
+++ src/test/java/com/albo/controlop/ControlopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albo.controlop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
