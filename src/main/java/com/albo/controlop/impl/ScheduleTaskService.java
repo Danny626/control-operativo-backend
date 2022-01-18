01e823d341e50df8a1362ba0f9234744c7f43865
Danny <live626@gmail.com>
2022-01-18 15:42:05 -0400

    feat: reprogramar tareas desde bd

--- src/main/java/com/albo/controlop/impl/ScheduleTaskService.java
+++ src/main/java/com/albo/controlop/impl/ScheduleTaskService.java
@@ -1,20 +1,38 @@
 package com.albo.controlop.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledFuture;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
+import com.albo.controlop.model.Tarea;
+import com.albo.controlop.service.ITareaService;
+
 @Service
 public class ScheduleTaskService {
+	
+	private static final Logger LOGGER = LogManager.getLogger(ScheduleTaskService.class);
 
 	// Task Scheduler
     TaskScheduler scheduler;
+    
+    @Autowired
+	private ITareaService tareaService;
+    
+    @Autowired
+	private ScheduleTaskService scheduleTaskService;
+    
+    @Autowired
+	private ProcesoService procesoService;
 
     // A map for keeping scheduled tasks
     Map<Long, ScheduledFuture<?>> jobsMap = new HashMap<>();
@@ -42,6 +60,27 @@ public class ScheduleTaskService {
     // A context refresh event listener
     @EventListener({ContextRefreshedEvent.class})
     void contextRefreshedEvent() {
-        // Get all tasks from DB and reschedule them in case of context restarted
+    	// Get all tasks from DB and reschedule them in case of context restarted
+    	LOGGER.info("Reprogramando tareas desde BD...");
+    	List<Tarea> tareas = this.tareaService.findAll();
+    	
+    	for(Tarea tarea : tareas) {
+    		switch(tarea.getTipo()) {
+				case "CARGA_PARTES_SUMA": {
+					
+					this.scheduleTaskService.addTaskToScheduler(
+							tarea.getId(), 
+							() -> this.procesoService.procesoCargaParteSuma(tarea), 
+							tarea.getCron()
+					);
+					
+					LOGGER.info("Tarea creada -> " + tarea.getNombre());					
+					break;
+				}
+				default: {
+					LOGGER.error("Tipo de tarea inexistente");
+				}
+			}
+    	}
     }
 }
