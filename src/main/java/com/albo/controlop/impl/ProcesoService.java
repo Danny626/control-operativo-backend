c9d80e522f3a4857cbc16939192bb9691c7e4249
Danny <live626@gmail.com>
2022-01-19 12:02:59 -0400

    fix: problema en  lectura de texto fecha en to y from en paramsMisPartesSuma

--- src/main/java/com/albo/controlop/impl/ProcesoService.java
+++ src/main/java/com/albo/controlop/impl/ProcesoService.java
@@ -3,6 +3,7 @@ package com.albo.controlop.impl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import com.albo.controlop.dto.ResultadoRegistroPartesSuma;
 import com.albo.controlop.model.Tarea;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Service
 public class ProcesoService {
@@ -22,17 +24,24 @@ public class ProcesoService {
 	
 	@Autowired
 	private SumaController sumaController;
+	
+	@Bean 
+	ObjectMapper objectMapper() { 
+		ObjectMapper objectMapper = new ObjectMapper(); 
+		objectMapper.registerModule(new JavaTimeModule()); 
+		return objectMapper; 
+	}
 
 	// realiza el proceso de carga de partes Suma
 	public void procesoCargaParteSuma(Tarea tarea) {
 		
 		BodyRegistroPartesSuma bodyRegistroPartesSuma = new BodyRegistroPartesSuma();
-		ObjectMapper objectMapper = new ObjectMapper();
+//		ObjectMapper objectMapper = new ObjectMapper();
 		
 //		Tarea tarea = this.tareaService.buscarPorNombre(codRecinto, tipoTarea);
 		
 		try {
-			bodyRegistroPartesSuma = objectMapper.readValue(tarea.getBody(), BodyRegistroPartesSuma.class);
+			bodyRegistroPartesSuma = this.objectMapper().readValue(tarea.getBody(), BodyRegistroPartesSuma.class);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			LOGGER.error("Error. El cuerpo para el registro de partes suma no es correcto");
